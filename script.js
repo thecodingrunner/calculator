d673f3b7db566faccf9af5a69284149b030ddd68
Finn Moffett <finn.moffett@gmail.com>
2024-01-21 16:23:56 +0000

    Add keyboard support
    Add keyboard support using a similar switch case for each key event.

--- script.js
+++ script.js
@@ -9,12 +9,86 @@ let a;
 let b;
 
 
+
+Node,addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case '+':
+      operators('+');
+      break;
+    case '-':
+      operators('-');
+      break;
+    case '*':
+      operators('*')
+      break;
+    case '/':
+      operators('/')
+      break;
+    case '1':
+      numbers(1);
+      break;
+    case '2':
+      numbers(2);
+      break;
+    case '3':
+      numbers(3);
+      break;
+    case '4':
+      numbers(4);
+      break;
+    case '5':
+      numbers(5);
+      break;
+    case '6':
+      numbers(6);
+      break;
+    case '7':
+      numbers(7);
+      break;
+    case '8':
+      numbers(8);
+      break;
+    case '9':
+      numbers(9);
+      break;
+    case '0':
+      numbers(0);
+      break;
+    case '=':
+      equals();
+      break;
+    case 'Enter':
+      equals();
+      break;
+    case 'c': 
+      display.textContent = '';
+      a = undefined;
+      op = undefined;
+      b = undefined;
+      break;
+    case '.':
+      numbers('.');
+      break;
+    case 'Backspace':
+      backspace();
+  }
+});
+
+
 // Function for each number. Based on whether each item already exists, add to the expression appropriately.
 function numbers(number) {
   if (!op) {
-    (!a) ? a = number : a = Number(a.toString() + (number).toString());
+    if (number === '.') {
+      (!a) ? a = number : a = (a.toString() + (number).toString());
+    } else {
+      (!a) ? a = number : a = parseFloat(a.toString() + (number).toString());
+    }
   } else {
-    (!b) ? b = number : b = Number(b.toString() + (number).toString());
+    if (number === '.') {
+      (!b) ? b = number : b = (b.toString() + (number).toString());
+    } else {
+      (!b) ? b = number : b = parseFloat(b.toString() + (number).toString());
+    }
   }
   display.textContent += number.toString()
 }
@@ -34,6 +108,28 @@ if (a !== undefined && op !== undefined && b !== undefined) {
   display.textContent += operator;
   op = operator;
 }
+}
+
+function equals() {
+  display.textContent = '';
+  let answer = operate(a, op, b);
+  display.textContent = +answer.toFixed(3);
+  a = answer;
+  b = undefined;
+  op = undefined;
+}
+
+function backspace() {
+  if (a && !op && !b) {
+    display.textContent = display.textContent.replace(a.toString(), '')
+    a = undefined;
+  } else if (a && op && !b) {
+    display.textContent = display.textContent.replace(op.toString(), '')
+    op = undefined;
+  } else if (a && op && b) {
+    display.textContent = display.textContent.replace(b.toString(), '')
+    b = undefined;
+  }
 }
 
 document.addEventListener("click", (event) => {
@@ -82,12 +178,7 @@ document.addEventListener("click", (event) => {
       numbers(0);
       break;
     case 'equals':
-      display.textContent = '';
-      let answer = operate(a, op, b);
-      display.textContent = +answer.toFixed(3);
-      a = answer;
-      b = undefined;
-      op = undefined;
+      equals();
       break;
     case 'clear': 
       display.textContent = '';
@@ -95,6 +186,11 @@ document.addEventListener("click", (event) => {
       op = undefined;
       b = undefined;
       break;
+    case 'decimal':
+      numbers('.');
+      break;
+    case 'backspace':
+      backspace();
   }
 });
 
@@ -110,3 +206,4 @@ function operate(a, op, b) {
     return a / b
   };
 };
+
